68ccfe51de23868eab8d507af1ad575900a62adf
Henry SU <hsu2@slb.com>
2020-04-09 20:35:01 +0800

    add cdk dependency

--- frontend/src/app/material/material.module.ts
+++ frontend/src/app/material/material.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
