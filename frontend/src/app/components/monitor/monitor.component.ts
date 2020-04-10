f52de1edbc8379ba1ced5605cbe0966c248dcb93
Henry SU <hsu2@slb.com>
2020-04-10 20:33:01 +0800

    img src to a url to monitor component

--- frontend/src/app/components/monitor/monitor.component.ts
+++ frontend/src/app/components/monitor/monitor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-monitor',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./monitor.component.scss']
 })
 export class MonitorComponent implements OnInit {
+  @Input() monitorUrl: string;
+  @Input() width: number;
+  @Input() height: number;
 
   constructor() { }
 
