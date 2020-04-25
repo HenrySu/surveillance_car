374e8cb1d642207a44c8352f613c00efe17b7f80
Henry SU <hsu2@slb.com>
2020-04-25 14:25:39 +0800

    add drag observable

--- frontend/src/app/components/joystick/joystick.component.ts
+++ frontend/src/app/components/joystick/joystick.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
+import { fromEvent, merge, Observable } from 'rxjs';
+import { skipUntil, takeWhile, takeUntil, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-joystick',
   templateUrl: './joystick.component.html',
   styleUrls: ['./joystick.component.scss']
 })
-export class JoystickComponent implements OnInit {
+export class JoystickComponent implements OnInit, AfterViewInit {
+  @ViewChild("joystickHandle") private joystickHandleElement: ElementRef;
+  joystickStart$: Observable<PointerEvent>;
+  joystickStop$: Observable<PointerEvent>;
+  joystickMove$: Observable<PointerEvent>;
+  joystickDrag$: Observable<PointerEvent>;
+  // joystickMove$: Observable<PointerEvent>;
+  private get HandleElement() {
+    return this.joystickHandleElement.nativeElement;
+  }
 
-  constructor() { }
+  constructor(private render: Renderer2) { }
+
+  ngAfterViewInit(): void {
+    this.joystickStart$ = fromEvent(this.HandleElement, "pointerdown");
+    this.joystickStop$ = fromEvent(this.HandleElement, "pointerup");
+    this.joystickMove$ = fromEvent(this.HandleElement, "pointermove");
+
+    this.joystickDrag$ = this.joystickStart$.pipe(
+      mergeMap(_=> this.joystickMove$.pipe(takeUntil(this.joystickStop$)))
+    );
+  }
 
   ngOnInit(): void {
   }
+
 
 }
