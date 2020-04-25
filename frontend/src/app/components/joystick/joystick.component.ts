a3dfb958206cfaac3229cfea16f5456c938e3148
Henry SU <hsu2@slb.com>
2020-04-25 21:40:55 +0800

    normalize percentage vector

--- frontend/src/app/components/joystick/joystick.component.ts
+++ frontend/src/app/components/joystick/joystick.component.ts
@@ -41,8 +41,8 @@ export class JoystickComponent implements OnInit, AfterViewInit {
   }
 
   private getPercentageVector(event: PointerEvent): Vector2 {
-    const x = this.joystickWidth === 0 ? 0 : event.offsetX / this.joystickWidth;
-    const y = this.joystickHeight === 0 ? 0 : event.offsetY / this.joystickHeight;
+    const x = this.joystickWidth === 0 ? 0 : (event.offsetX / this.joystickWidth - 0.5) * 2;
+    const y = this.joystickHeight === 0 ? 0 : (0.5 - event.offsetY / this.joystickHeight) * 2;
     return new Vector2(x, y);
   }
   private getEventStream<T extends Event>(nativeElement: any, eventName: string): Observable<T> {
