16a3567d77218d6ce53ad6ec172372450aeec9fb
Henry SU <hsu2@slb.com>
2020-04-25 21:43:47 +0800

    add comment to vector2

--- frontend/src/app/models/vector2.ts
+++ frontend/src/app/models/vector2.ts
@@ -1,3 +1,13 @@
+/// percentage vector 2
+/*
+       1 ^
+         |
+-1       |
+<--------0-------->
+         |        1
+         |
+       -1v
+*/
 export class Vector2 {
     constructor(public x:number, public y:number){}
     // constructor(public angleRadian: number = 0, public distance: number = 0) {
