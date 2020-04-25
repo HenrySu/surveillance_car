50ed799694bfeebddbcd8db95582f2982de11ca6
Henry SU <hsu2@slb.com>
2020-04-25 17:25:22 +0800

    add strip default to strip off event default behavior

--- frontend/src/app/components/joystick/joystick.component.ts
+++ frontend/src/app/components/joystick/joystick.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 import { fromEvent, merge, Observable } from 'rxjs';
-import { skipUntil, takeWhile, takeUntil, mergeMap } from 'rxjs/operators';
+import { skipUntil, takeWhile, takeUntil, mergeMap, map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-joystick',
@@ -21,13 +21,25 @@ export class JoystickComponent implements OnInit, AfterViewInit {
   constructor(private render: Renderer2) { }
 
   ngAfterViewInit(): void {
-    this.joystickStart$ = fromEvent(this.HandleElement, "pointerdown");
-    this.joystickStop$ = fromEvent(this.HandleElement, "pointerup");
-    this.joystickMove$ = fromEvent(this.HandleElement, "pointermove");
+    this.joystickStart$ = this.getEventStream(this.HandleElement, "pointerdown");
+    this.joystickStop$ = this.getEventStream(this.HandleElement, "pointerup");
+    this.joystickMove$ = this.getEventStream(this.HandleElement, "pointermove");
 
     this.joystickDrag$ = this.joystickStart$.pipe(
-      mergeMap(_=> this.joystickMove$.pipe(takeUntil(this.joystickStop$)))
+      mergeMap(_ => this.joystickMove$.pipe(takeUntil(this.joystickStop$)))
     );
+
+    this.joystickDrag$.subscribe(x => console.log(x))
+  }
+
+  private getEventStream(nativeElement: any, eventName: string): Observable<PointerEvent> {
+    return <Observable<PointerEvent>>fromEvent(nativeElement, eventName)
+      .pipe(map(this.stripDefault));
+  }
+
+  private stripDefault(event: Event): Event {
+    event.preventDefault();
+    return event;
   }
 
   ngOnInit(): void {
