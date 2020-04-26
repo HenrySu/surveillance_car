820745fc4be91183396ddfcb607042b1d555eff2
Henry SU <hsu2@slb.com>
2020-04-26 15:47:42 +0800

    add set RPM method

--- backend/src/models/wheel.ts
+++ backend/src/models/wheel.ts
@@ -1,7 +1,22 @@
+import { CarMovement } from "./car-movement";
+
 export class Wheel {
+    static readonly DutyCycle = 150;
     constructor(private forwardPin: number,
         private backwardPin: number,
         pwmPin: number) {
 
+    }
+
+    move(vector:CarMovement) {
+        this.setRPM(vector);
+    }
+
+    private setRPM(vector: CarMovement) {
+        this.getDutyCycle(vector.yRatio);
+    }
+
+    private getDutyCycle(ratio:number) {
+        Math.abs(ratio) * Wheel.DutyCycle;
     }
 }
