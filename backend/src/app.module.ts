0b07eb36960ae811fa43d82a5194238d1fb3607d
Henry SU <hsu2@slb.com>
2020-04-26 11:37:49 +0800

    add monitor controller and service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CarController } from './car/car.controller';
 import { CarService } from './car/car.service';
+import { MonitorController } from './monitor/monitor.controller';
+import { MonitorService } from './monitor/monitor.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, CarController],
-  providers: [AppService, CarService],
+  controllers: [AppController, CarController, MonitorController],
+  providers: [AppService, CarService, MonitorService],
 })
 export class AppModule {}
