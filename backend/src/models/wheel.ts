840fd717a4e1f556e7be90f422fa4791984abe4f
HenrySU <suhang@hotmail.com>
2020-04-26 15:07:38 +0000

    write forward and backward  gpio based on direction

--- backend/src/models/wheel.ts
+++ backend/src/models/wheel.ts
@@ -1,10 +1,20 @@
+import { DigitalOutput } from "raspi-gpio";
+import * as pigpio from "pigpio";
+import Gpio = pigpio.Gpio;
+
 type Direction = "forward" | "backward";
 export class Wheel {
     static readonly DutyCycle = 150;
-    constructor(private forwardPin: number,
-        private backwardPin: number,
-        pwmPin: number) {
+    private forwardGpio:Gpio;
+    private backwardGpio:Gpio;
+    private pwmGpio:Gpio;
 
+    constructor(forwardPin: number,
+        backwardPin: number,
+        pwmPin: number) {
+        this.forwardGpio = new Gpio(forwardPin, {mode:Gpio.OUTPUT});
+        this.backwardGpio = new Gpio(backwardPin, {mode:Gpio.OUTPUT});
+        this.pwmGpio = new Gpio(pwmPin, {mode:Gpio.OUTPUT});
     }
 
     move(speedRatio: number) {
@@ -13,17 +23,26 @@ export class Wheel {
     }
 
     private setDirection(speedRatio: number): void {
-        this.getDirection(speedRatio);
+        const direction = this.getDirection(speedRatio);
+        if(direction === "forward"){
+            this.forwardGpio.digitalWrite(1);
+            this.backwardGpio.digitalWrite(0);
+        }
+        else{
+            this.forwardGpio.digitalWrite(0);
+            this.backwardGpio.digitalWrite(1);
+        }
     }
 
     private setSpeed(speedRatio: number): void {
-        this.getDutyCycle(speedRatio);
+        const dutyCycle = this.getDutyCycle(speedRatio);
+        this.pwmGpio.pwmWrite(dutyCycle);
     }
 
     private getDirection(ratio: number): Direction {
         return ratio > 0 ? "forward" : "backward";
     }
-    private getDutyCycle(ratio: number) {
-        Math.abs(ratio) * Wheel.DutyCycle;
+    private getDutyCycle(ratio: number):number {
+        return Math.abs(ratio) * Wheel.DutyCycle;
     }
 }
