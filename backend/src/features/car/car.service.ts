aeee49d7cc71c591a0aff6c322b320fe35f0e247
Henry SU <hsu2@slb.com>
2020-04-26 13:32:21 +0800

    pass car factory to car service

--- backend/src/features/car/car.service.ts
+++ backend/src/features/car/car.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { CarFactoryService } from 'src/hardware/factories/car-factory.service';
+import { Car } from 'src/models';
 import { CarMovement } from 'src/models/car-movement';
 
 @Injectable()
 export class CarService {
+    private car: Car;
+    constructor(carFactory: CarFactoryService) {
+        // this.car = carFactory.createCar(null);
+    }
     move(moveVector: CarMovement) {
-        
+
     }
     getCarInfo() {
         return "Henry's surveillance car";
