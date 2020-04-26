899ec162ee2d7b1e4a1a058ceff0db87934ba1e5
Henry SU <hsu2@slb.com>
2020-04-26 10:42:10 +0800

    add throttleTime to limit steer stream bandwidth

--- frontend/src/app/components/control-desk/control-desk.component.ts
+++ frontend/src/app/components/control-desk/control-desk.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, OnDestroy, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { CameraService, CarService } from 'src/app/services';
 import { JoystickComponent } from '../joystick/joystick.component';
+import { throttleTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-control-desk',
@@ -22,8 +23,12 @@ export class ControlDeskComponent implements OnDestroy, AfterViewInit {
 
   ngAfterViewInit(): void {
     this.subscriptions.push(
-      this.carSteer.joystickPositionPercentageVector$.subscribe(x => console.log(`car`)),
-      this.cameraSteer.joystickPositionPercentageVector$.subscribe(x => console.log(`camera`)),
+      this.carSteer.joystickPositionPercentageVector$
+        .pipe(throttleTime(150))
+        .subscribe(x => console.log(`car`)),
+      this.cameraSteer.joystickPositionPercentageVector$
+        .pipe(throttleTime(150))
+        .subscribe(x => console.log(`camera`)),
     );
   }
 }
