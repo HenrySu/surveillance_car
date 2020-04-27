c7cb94722e858e666e68c17c86d11559efa7d6b5
HenrySU <suhang@hotmail.com>
2020-04-27 13:57:22 +0000

    add camera factory

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,6 +8,7 @@ import { MonitorController } from './features/monitor/monitor.controller';
 import { MonitorService } from './features/monitor/monitor.service';
 import { CarFactoryService } from './hardware/factories/car-factory.service';
 import { WheelFactoryService } from './hardware/factories/wheel-factory.service';
+import { CameraFactoryService } from './hardware/factories/camera-factory.service';
 import carConfig from "./config/car-configuration";
 
 @Module({
@@ -16,6 +17,6 @@ import carConfig from "./config/car-configuration";
       load: [carConfig]
     })],
   controllers: [AppController, CarController, MonitorController],
-  providers: [AppService, CarService, MonitorService, CarFactoryService, WheelFactoryService],
+  providers: [AppService, CarService, MonitorService, CarFactoryService, WheelFactoryService, CameraFactoryService],
 })
 export class AppModule { }
