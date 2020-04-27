7817621905cac0d0f9c244308f899625135cb857
HenrySU <suhang@hotmail.com>
2020-04-27 06:22:01 +0000

    update car movement, make move according to joystick direction

--- backend/src/models/car.ts
+++ backend/src/models/car.ts
@@ -6,11 +6,28 @@ export class Car {
     }
 
     move(vector: CarMovement): void {
+        const [leftRatio, rightRatio] = this.getLeftRightRatios(vector);
+        this.leftWheel.move(leftRatio * vector.distanceRatio);
+        this.rightWheel.move(rightRatio * vector.distanceRatio);
+    }
+
+    getLeftRightRatios(vector: CarMovement) {
         const moveRatio = Math.cos(vector.arc);
         const turnRatio = Math.sin(vector.arc);
         let [leftRatio, rightRatio] = [moveRatio, moveRatio];
-        vector.arc > 0 && vector.arc < Math.PI ? leftRatio += turnRatio : rightRatio += turnRatio;
-        this.leftWheel.move(leftRatio * vector.distanceRatio);
-        this.rightWheel.move(rightRatio * vector.distanceRatio);
+        if (vector.arc > 0 && vector.arc <= Math.PI / 2) {
+            leftRatio += turnRatio;
+        }
+        else if (vector.arc > Math.PI / 2 && vector.arc <= Math.PI) {
+            leftRatio -= turnRatio;
+        }
+        else if (vector.arc > Math.PI && vector.arc <= Math.PI / 2 * 3) {
+            rightRatio += turnRatio;
+        }
+        else {
+            rightRatio -= turnRatio;
+        }
+
+        return [leftRatio, rightRatio];
     }
 }
--- backend/src/models/wheel.ts
+++ backend/src/models/wheel.ts
@@ -4,16 +4,16 @@ import Gpio = pigpio.Gpio;
 type Direction = "forward" | "backward";
 export class Wheel {
     static readonly DutyCycle = 255;
-    private forwardGpio:Gpio;
-    private backwardGpio:Gpio;
-    private pwmGpio:Gpio;
+    private forwardGpio: Gpio;
+    private backwardGpio: Gpio;
+    private pwmGpio: Gpio;
 
     constructor(forwardPin: number,
         backwardPin: number,
         pwmPin: number) {
-        this.forwardGpio = new Gpio(forwardPin, {mode:Gpio.OUTPUT});
-        this.backwardGpio = new Gpio(backwardPin, {mode:Gpio.OUTPUT});
-        this.pwmGpio = new Gpio(pwmPin, {mode:Gpio.OUTPUT});
+        this.forwardGpio = new Gpio(forwardPin, { mode: Gpio.OUTPUT });
+        this.backwardGpio = new Gpio(backwardPin, { mode: Gpio.OUTPUT });
+        this.pwmGpio = new Gpio(pwmPin, { mode: Gpio.OUTPUT });
     }
 
     move(speedRatio: number) {
@@ -23,11 +23,11 @@ export class Wheel {
 
     private setDirection(speedRatio: number): void {
         const direction = this.getDirection(speedRatio);
-        if(direction === "forward"){
+        if (direction === "forward") {
             this.forwardGpio.digitalWrite(1);
             this.backwardGpio.digitalWrite(0);
         }
-        else{
+        else {
             this.forwardGpio.digitalWrite(0);
             this.backwardGpio.digitalWrite(1);
         }
@@ -41,7 +41,7 @@ export class Wheel {
     private getDirection(ratio: number): Direction {
         return ratio > 0 ? "forward" : "backward";
     }
-    private getDutyCycle(ratio: number):number {
-        return Math.floor(Math.abs(ratio) * Wheel.DutyCycle);
+    private getDutyCycle(ratio: number): number {
+        return Math.min(Wheel.DutyCycle, Math.floor(Math.abs(ratio) * Wheel.DutyCycle));
     }
 }
