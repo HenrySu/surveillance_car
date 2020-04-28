d0c262487bdb15146e60612e790b501e0cdf24df
HenrySU <suhang@hotmail.com>
2020-04-28 03:27:28 +0000

    initialize pwm in camera

--- backend/src/models/camera.ts
+++ backend/src/models/camera.ts
@@ -1,3 +1,28 @@
+import * as i2cBus from "i2c-bus";
+import * as pca9685 from "pca9685"
+import PCA9685Driver = pca9685.Pca9685Driver;
+
 export class Camera {
-    constructor(i2cPin: number, horizontalPin: number, verticalPin: number) { }
+    private pwm: PCA9685Driver;
+
+    constructor(i2cPin: number, private horizontalPin: number, private verticalPin: number) {
+        this.pwm = new PCA9685Driver(
+            {
+                i2c: i2cBus.openSync(1),
+                address: i2cPin,
+                frequency: 50,
+                debug: false
+            }, function (err) {
+                throw err;
+            });
+    }
+    
+    private getHorizontalDutyCycle(): number {
+        return 0;
+    }
+
+    private getVerticalDutyCycle(): number {
+        return 0;
+    }
+
 }
