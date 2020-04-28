ec695b356fc1961e2e1e48e5dc87115d10899374
HenrySU <suhang@hotmail.com>
2020-04-28 05:48:09 +0000

    add get duty cycle method to servo

--- backend/src/hardware/models/servo.ts
+++ backend/src/hardware/models/servo.ts
@@ -5,11 +5,26 @@ export class Servo {
     private _angle: number;
     set angle(value: number) {
         // clap angle to [0,180] as servo only serves that range
-        this._angle = Math.max(Math.min(value, 180), 0);;
+        this._angle = Math.max(Math.min(value, 180), 0);
+        this.pwm.setDutyCycle(this.channelNum, this.getDutyCycle(), 0);
     }
 
-    constructor(channelNum: number,
-        pwm: PCA9685Driver) {
+    constructor(private channelNum: number,
+        private pwm: PCA9685Driver,
+        private angleStep: number = 5) {
         this._angle = 90;
     }
+
+    increaseAngle() {
+        this.angle = this.angle + this.angleStep;
+    }
+
+    decreaseAngle() {
+        this.angle = this.angle - this.angleStep;
+    }
+
+    getDutyCycle(): number {
+        return 0;
+    }
+
 }
