200975d7d266315b6a5cb38d77843108a01f975f
Henry SU <hsu2@slb.com>
2020-05-05 10:46:25 +0800

    change formgroup input binding

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,17 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './components/app/app.component';
-import { MaterialModule } from './material';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgJoystickModule } from "ng-joystick";
-import { ControlDeskComponent } from './components/control-desk/control-desk.component';
-import { MonitorComponent } from './components/monitor/monitor.component';
-import { JoystickComponent } from './components/joystick/joystick.component';
-import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 import { AuthModule } from './auth/auth.module';
+import { AppComponent } from './components/app/app.component';
+import { ControlDeskComponent } from './components/control-desk/control-desk.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { JoystickComponent } from './components/joystick/joystick.component';
+import { MonitorComponent } from './components/monitor/monitor.component';
+import { MaterialModule } from './material';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { AuthModule } from './auth/auth.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MaterialModule,
     NgJoystickModule,
     HttpClientModule,
