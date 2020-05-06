ba50f6819a4f6512ae83899c7173c037aefb4c8a
Henry SU <hsu2@slb.com>
2020-05-06 22:28:46 +0800

    add auth reducer

---
+++ frontend/src/app/auth/reducers/auth.reducer.ts
@@ -0,0 +1,30 @@
+import { createReducer, on } from '@ngrx/store';
+import { AuthApiActions } from '../actions';
+import { loginResponse } from '../actions/auth-api.actins';
+
+export const featureKey = "Auth";
+
+export interface State {
+    token: string;
+}
+
+export const initialState: State = {
+    token: ""
+}
+
+export const reducers = createReducer(
+    initialState,
+    on(AuthApiActions.loginResponse,
+        (state, loginResponse) => {
+            if (loginResponse.isOK) {
+                return {
+                    ...state,
+                    token: loginResponse.token
+                };
+            }
+            return {
+                ...state,
+                token: ""
+            };
+        }),
+);
