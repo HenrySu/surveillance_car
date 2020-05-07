4971527ec0dba0213d3383c04b6db3d1dce2bc9e
Henry SU <hsu2@slb.com>
2020-05-07 09:49:10 +0800

    add index.ts to auth folder

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,18 +3,18 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth';
 import { AppComponent } from './components/app/app.component';
 import { ControlDeskComponent } from './components/control-desk/control-desk.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { JoystickComponent } from './components/joystick/joystick.component';
 import { MonitorComponent } from './components/monitor/monitor.component';
 import { MaterialModule } from './material';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-import { EffectsModule } from '@ngrx/effects';
 import { metaReducers } from "./reducers";
 
 @NgModule({
