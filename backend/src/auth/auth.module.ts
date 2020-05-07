c198477e744d48a1ffb43dcc4f5abef1093fbe60
Henry SU <hsu2@slb.com>
2020-05-07 21:28:18 +0800

    add validate user method to auth service

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
 
 @Module({
-  providers: [AuthService]
+  providers: [AuthService],
+  imports: [UsersModule]
 })
-export class AuthModule {}
+export class AuthModule { }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private usersSvc: UsersService) { }
+
+    async validateUser(username: string, password: string): Promise<any> {
+        const user = await this.usersSvc.findOne(username);
+        if (user?.password === password) {
+            return username;
+        }
+
+        return undefined;
+    }
+}
