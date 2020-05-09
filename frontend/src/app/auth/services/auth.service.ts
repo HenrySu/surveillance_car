e358fac270bc43ba4879254d0ee63bd2027d010a
Henry SU <hsu2@slb.com>
2020-05-09 18:11:34 +0800

    inject http client to auth service

--- frontend/src/app/auth/services/auth.service.ts
+++ frontend/src/app/auth/services/auth.service.ts
@@ -1,24 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Credentials, LoginResult } from '../models';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   login(credentials: Credentials): Observable<LoginResult> {
-    if(credentials.username === "henry"){
+    if (credentials.username === "henry") {
       return of({
-        isOK:true,
-        errorMessage:"",
-        token:"whatever"
+        isOK: true,
+        errorMessage: "",
+        token: "whatever"
       })
     }
     return of({
-      isOK:false,
+      isOK: false,
       errorMessage: "username or password is invalid",
       token: ``
     });
