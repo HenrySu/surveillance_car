91452ced90cff86477d039600a3ada201a25a8b0
Henry SU <hsu2@slb.com>
2020-05-09 09:58:14 +0800

    add local auth guard that inherit from AuthGuard("local")

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post()
+  sayHello(): string {
+    return "Hello post";
   }
 }
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './local-auth.guard';
 import { LocalStragety } from './stragety.local';
 
 @Module({
-  providers: [AuthService, LocalStragety],
+  providers: [AuthService, LocalStragety, LocalAuthGuard],
   imports: [UsersModule, PassportModule]
 })
 export class AuthModule { }
